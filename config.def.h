580502329a4543a243c51d0f6d8c9a4189301808
Hans Chaumont <hans.chaumont@gmail.com>
2024-05-09 12:41:01 -0700

    Remove unused colors

--- config.def.h
+++ config.def.h
@@ -8,10 +8,6 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_yellow[]      = "#d79921";
 static const char col_bg[]          = "#282828";
